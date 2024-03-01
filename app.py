9ee4e6f7069b6d50a488108916c04e4d04f93a3a
lisatordai <lisatordai@gmail.com>
2024-03-01 13:35:33 -0600

    bucket

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
 from flask import Flask, render_template, request
 #from flask_sqlalchemy import SQLAlchemy
+#pip3 install boto3 flask
+import boto3
+from flask import Flask, render_template
 
 application = Flask(__name__, template_folder="app/HTML")
+
+#for bucket access
+AWS_ACCESS_KEY_ID = 'AKIAZI2LHWQSKEKULJUU'
+AWS_SECRET_ACCESS_KEY = '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'
+BUCKET_NAME = '422p1bucket'
+s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
 @application.route("/")
 def login():
@@ -16,8 +25,21 @@ def home():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
+        
+    response = s3.list_objects_v2(Bucket=BUCKET_NAME)
 
-    return  render_template('home.html')
+    #get 10 images
+    objects = response['Contents'][:10]
+
+
+    images = []
+    for obj in objects:
+        image_url = s3.generate_presigned_url('get_object', Params={'Bucket': BUCKET_NAME, 'Key': obj['Key']})
+        caption = obj['Key']  # You may need to modify this depending on how your images are named
+        images.append({'url': image_url, 'caption': caption})
+
+    return  render_template('home.html', images=images)
+
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0', port='8080', debug=True)
