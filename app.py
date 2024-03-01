b6fda7030242d68b53979f2c13594554556ec3ac
EC2 Default User <ec2-user@ip-172-31-5-243.us-east-2.compute.internal>
2024-03-01 18:31:29 +0000

    Made a few modifications to app.py so we connect to our website with gunicorn and nginx

--- app.py
+++ app.py
@@ -1,17 +1,17 @@
 from flask import Flask, render_template, request
 #from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__, template_folder="app/HTML")
+application = Flask(__name__, template_folder="app/HTML")
 
-@app.route("/")
+@application.route("/")
 def login():
     return  render_template('login.html')
 
-@app.route("/new_account")
+@application.route("/new_account")
 def new_login():
     return render_template('new_account.html')
 
-@app.route("/home", methods=['GET', 'POST'])
+@application.route("/home", methods=['GET', 'POST'])
 def home():
     if request.method == 'POST':
         username = request.form['username']
@@ -20,4 +20,4 @@ def home():
     return  render_template('home.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    application.run(host='0.0.0.0', port='8080', debug=True)
