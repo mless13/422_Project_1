47a6dc2888d998c3ea7e760c9bb4a0abe68dc30a
lisatordai <lisatordai@gmail.com>
2024-03-01 14:12:16 -0600

    fix

--- app.py
+++ app.py
@@ -25,28 +25,7 @@ def home():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        
-    try:
 
-If your images are named in the format burger.jpg, cake.jpg, etc., and you want to use the image filenames as the alt attributes, you can directly use the image filenames without any modification. Here's how you can adjust the code:
-
-python
-Copy code
-from flask import Flask, render_template
-import boto3
-
-app = Flask(__name__)
-
-# AWS S3 credentials
-AWS_ACCESS_KEY_ID = 'your_access_key_id'
-AWS_SECRET_ACCESS_KEY = 'your_secret_access_key'
-BUCKET_NAME = 'your_bucket_name'
-
-# Initialize Boto3 S3 client
-s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
-
-@app.route("/home")
-def home():
     try:
         response = s3.list_objects_v2(Bucket=BUCKET_NAME)
         objects = response.get('Contents', [])[:10]
