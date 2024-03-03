cfd45ee618b45f318d537fc0487e3662e8fb9b24
EC2 Default User <ec2-user@ip-172-31-5-243.us-east-2.compute.internal>
2024-03-03 21:33:16 +0000

    Latest Version, has images displayed. Working on filtering images then uploads.

--- app.py
+++ app.py
@@ -7,9 +7,10 @@ from flask import Flask, render_template
 application = Flask(__name__, template_folder="app/HTML")
 
 #for bucket access
-AWS_ACCESS_KEY_ID = 'AKIAZI2LHWQSKEKULJUU'
-AWS_SECRET_ACCESS_KEY = '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'
+AWS_ACCESS_KEY_ID = 'AKIAZI2LHWQSB2BAXAKG'
+AWS_SECRET_ACCESS_KEY = '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'
 BUCKET_NAME = '422p1bucket'
+EXPIRATION_TIME = 3600
 s3 = boto3.client('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
 @application.route("/")
@@ -20,12 +21,8 @@ def login():
 def new_login():
     return render_template('new_account.html')
 
-@application.route("/home")
-def home():
-#    if request.method == 'POST':
- #       username = request.form['username']
-  #      password = request.form['password']
-    
+@application.route("/home", methods=['POST', 'GET'])
+def home():   
     try:
         print("TRY")
         response = s3.list_objects_v2(Bucket=BUCKET_NAME)
@@ -33,14 +30,14 @@ def home():
 
         images = []
         for obj in objects:
-            image_url = s3.generate_presigned_url('get_object', Params={'Bucket': BUCKET_NAME, 'Key': obj['Key']})
+            
+            image_url = f'https://{BUCKET_NAME}.s3.amazonaws.com/{obj["Key"]}'
+            # image_url = s3.generate_presigned_url('get_object', Params={'Bucket': BUCKET_NAME, 'Key': obj['Key']}, ExpiresIn=EXPIRATION_TIME)
             caption = obj['Key']  # You may need to modify this depending on how your images are named
             images.append({'url': image_url, 'caption': caption})
     except Exception as e:
-        print("Exception")
         return render_template('home.html')
-      #  return f"Error: {str(e)}", 500
-    print("End of try/catch")
+        return f"Error: {str(e)}", 500
     return  render_template('home.html', images=images)
 
 
